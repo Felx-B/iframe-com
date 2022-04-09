b370d60081fb3f7272352d3bcbbf1c275e7da055
Félix BOROT <fborot@divalto.com>
2022-04-09 22:36:43 +0200

    create & init iframes

--- static/script.js
+++ static/script.js
@@ -1,7 +1,11 @@
 const mainContainer = document.getElementById("main-container");
 const mainTitle = document.getElementById("title");
 
-// On main page init -> create an iframe
+let parentId;
+let myId;
+let childrenIds = [];
+
+// On main page init -> create first iframe
 if (window == window.parent) {
   mainPageInit();
 } else {
@@ -9,37 +13,46 @@ if (window == window.parent) {
 }
 
 function mainPageInit() {
-  createIframe();
+  const firstIframeId = createIframe("mainPage");
+  document.getElementById("children").innerText += ` ${firstIframeId}`;
   window.onmessage = (m) => {
     console.log("main page", m);
-    if (m.ports && m.data === "createChild") {
-      const id = createIframe();
+    if (m.ports && m.data.type === "createChild") {
+      const id = createIframe(m.data.parent);
       m.ports[0].postMessage(id);
     }
   };
 }
 
-function createIframe() {
+// Create iframe, generate id, set id in iframe, and return id to the caller
+function createIframe(parentId) {
   const myIframe = document.createElement("iframe");
   myIframe.setAttribute("src", window.location.href);
   mainContainer.appendChild(myIframe);
 
   const id = Math.random().toString().substring(2, 8);
   setTimeout(() => {
-    myIframe.contentWindow.postMessage({ id });
+    myIframe.contentWindow.postMessage({ type: "init", id, parent: parentId });
   }, 100);
   return id;
 }
 
+function setParent(id) {
+  mainTitle.innerHTML += ` My Parent is ${id}`;
+  parentId = id;
+}
+
 function setId(id) {
   mainTitle.innerHTML = `My ID is ${id}`;
+  myId = id;
 }
 
 function iframeInit() {
   onmessage = (e) => {
     console.log("in iframe", e);
-    if (e.data.id) {
+    if (e.data.type == "init") {
       setId(e.data.id);
+      setParent(e.data.parent);
     }
   };
 
@@ -52,7 +65,7 @@ function iframeInit() {
 async function createChild() {
   const childId = await askParentoToCreateChildIframe(window.parent);
   document.getElementById("children").innerText += ` ${childId}`;
-  // TODO stocker les ids de tous les enfants
+  childrenIds.push(childId);
 }
 
 function askParentoToCreateChildIframe(target) {
@@ -62,6 +75,8 @@ function askParentoToCreateChildIframe(target) {
     channel.port1.onmessage = (e) => resolve(e.data);
     // let iframe know we're expecting an answer
     // send it its own port
-    target.postMessage("createChild", "*", [channel.port2]);
+    target.postMessage({ type: "createChild", parent: myId }, "*", [
+      channel.port2,
+    ]);
   });
 }
