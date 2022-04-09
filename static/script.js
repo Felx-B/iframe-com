73533adb42e474c7071ae79b2600608f9c15b698
Félix BOROT <fborot@divalto.com>
2022-04-09 23:21:57 +0200

    remote property ok

--- static/script.js
+++ static/script.js
@@ -1,5 +1,10 @@
 const mainContainer = document.getElementById("main-container");
-const mainTitle = document.getElementById("title");
+const childrenDiv = document.getElementById("children");
+const idDiv = document.getElementById("id");
+const innerDiv = document.getElementById("inner");
+const parentDiv = document.getElementById("parent");
+
+let iframes = {};
 
 let parentId;
 let myId;
@@ -14,12 +19,20 @@ if (window == window.parent) {
 
 function mainPageInit() {
   const firstIframeId = createIframe("mainPage");
-  document.getElementById("children").innerText += ` ${firstIframeId}`;
-  window.onmessage = (m) => {
-    console.log("main page", m);
+  childrenDiv.innerText += firstIframeId;
+  idDiv.innerText += "mainPage";
+  window.onmessage = async (m) => {
     if (m.ports && m.data.type === "createChild") {
       const id = createIframe(m.data.parent);
       m.ports[0].postMessage(id);
+    }
+    if (m.ports && m.data.type == "getProperty") {
+      const result = await askTargetGetProperty(
+        iframes[m.data.childId],
+        m.data.childId,
+        m.data.propertyName
+      );
+      m.ports[0].postMessage(result);
     }
   };
 }
@@ -34,25 +47,21 @@ function createIframe(parentId) {
   setTimeout(() => {
     myIframe.contentWindow.postMessage({ type: "init", id, parent: parentId });
   }, 100);
+  iframes[id] = myIframe.contentWindow;
   return id;
-}
-
-function setParent(id) {
-  mainTitle.innerHTML += ` My Parent is ${id}`;
-  parentId = id;
-}
-
-function setId(id) {
-  mainTitle.innerHTML = `My ID is ${id}`;
-  myId = id;
 }
 
 function iframeInit() {
   onmessage = (e) => {
-    console.log("in iframe", e);
+    // console.log("in iframe", e);
     if (e.data.type == "init") {
-      setId(e.data.id);
-      setParent(e.data.parent);
+      idDiv.innerHTML += e.data.id;
+      myId = e.data.id;
+      parentDiv.innerHTML += e.data.parent;
+      parentId = e.data.parent;
+    }
+    if (e.data.type == "getProperty") {
+      e.ports[0].postMessage(window[e.data.propertyName]);
     }
   };
 
@@ -60,22 +69,47 @@ function iframeInit() {
   myBtn.onclick = () => createChild();
   myBtn.innerText = "Create Child";
   mainContainer.appendChild(myBtn);
+  window.innerId = Math.random().toString().substring(2, 8);
+  innerDiv.innerText += window.innerId;
 }
 
 async function createChild() {
-  const childId = await askParentoToCreateChildIframe(window.parent);
-  document.getElementById("children").innerText += ` ${childId}`;
+  const childId = await askParentoToCreateChildIframe();
+  childrenDiv.innerText += ` ${childId}`;
   childrenIds.push(childId);
+
+  const myBtn = document.createElement("button");
+  myBtn.onclick = () => getChildProperty(childId);
+  myBtn.innerText = `Get ${childId} property`;
+  mainContainer.appendChild(myBtn);
 }
 
-function askParentoToCreateChildIframe(target) {
+function askParentoToCreateChildIframe() {
   return new Promise((resolve) => {
     const channel = new MessageChannel();
     // this will fire when iframe will answer
     channel.port1.onmessage = (e) => resolve(e.data);
     // let iframe know we're expecting an answer
     // send it its own port
-    target.postMessage({ type: "createChild", parent: myId }, "*", [
+    window.parent.postMessage({ type: "createChild", parent: myId }, "*", [
+      channel.port2,
+    ]);
+  });
+}
+
+async function getChildProperty(childId) {
+  const result = await askTargetGetProperty(window.parent, childId, "innerId");
+  console.log(`myId is ${myId} innerId of iframe ${childId} is ${result}`);
+}
+
+function askTargetGetProperty(target, childId, propertyName) {
+  return new Promise((resolve) => {
+    const channel = new MessageChannel();
+    // this will fire when iframe will answer
+    channel.port1.onmessage = (e) => resolve(e.data);
+    // let iframe know we're expecting an answer
+    // send it its own port
+    target.postMessage({ type: "getProperty", childId, propertyName }, "*", [
       channel.port2,
     ]);
   });
